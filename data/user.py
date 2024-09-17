6caf30214ecd277e63228f3d01bff570949a1222
anton <a_widmark@hotmail.com>
2024-09-17 23:31:06 +0200

    updated user db models

--- data/user.py
+++ data/user.py
@@ -1,27 +1,37 @@
 
+import dataclasses
+
 from data import database as db
 
+@dataclasses.dataclass(frozen=True)
+class User:
+    id: int
+    username: str
 
 
-class User(db.INSTANCE.Model):
+class DbUser(db.INSTANCE.Model):
     id = db.INSTANCE.Column(db.INSTANCE.Integer, primary_key=True)
-    username = db.INSTANCE.Column(db.INSTANCE.String(), unique=True, nullable=False)
+    username = db.INSTANCE.Column(db.INSTANCE.String, unique=True, nullable=False)
 
-    def __repr__(self):
-        return f'<User {self.username}>'
+    @classmethod
+    def from_user(user: User) -> "DbUser":
+        return DbUser(**dataclasses.asdict(user))
+
+    def to_user(self) -> User:
+        return User(id=self.id, username=self.username)
 
 
 class UserApi():
-    def create_new(self, username: str) -> User:
-        new_user = User(username=username)
-        db.INSTANCE.session.add(new_user)
+
+    def insert_new(self, username: str) -> None:
+        db_user = DbUser(username=username)
+        db.INSTANCE.session.add(db_user)
         db.INSTANCE.session.commit()
 
-        return new_user
-        
-
     def get_by_username(self, username: str) -> User:
-        return User.query.filter_by(username=username).first()
+        db_user = DbUser.query.filter_by(username=username).first()
+        return db_user.to_user()
+
 
         
     
